d0098efe73a211cdcc9633cc79c4e1fa9abe87e5
Moustapha Diao <j.mamba1524@gmail.com>
2019-09-14 18:08:55 +0000

    acces token

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,8 +2,7 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: ENV["ACCESS_TOKEN"],
-  subscribed_fields: %w[feed mention name]
+  access_token: ENV["ACCESS_TOKEN"]
  )
 
 Bot.on :message do |message|
