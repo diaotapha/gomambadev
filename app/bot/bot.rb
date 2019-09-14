5aff1388b37ddd67de837ee930a0ba296281f054
Moustapha Diao <j.mamba1524@gmail.com>
2019-09-14 19:07:58 +0000

    acces token

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
+Facebook::Messenger::Subscriptions.subscribe(access_token: "EAALxmRb1J7sBAFS6ZCj8CFzjZA9CZCAZBCOO1H2ZANVpNPnYOdtlaNguQ0hzjZChDHLRc3isr2RHfL4sBHzjrnjf7yCAxkYIDVmd9Mdwe88PKCCeTSFjw7ji8caBXjjp3mxSVnzZAsPbnOBvwpxIUZAZAtJclIPoNr20NwotNovRg3gZDZD")
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
