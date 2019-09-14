2d77b08c3e0d2a9cb48ce7cfc3c7c3f9a06483ec
Moustapha Diao <j.mamba1524@gmail.com>
2019-09-14 20:09:42 +0000

    bot initiation

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,6 +4,13 @@ include Facebook::Messenger
 # Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'], subscribed_fields: [])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  
+  if message.text.include? "Bonjour"
+  	message.reply(text: 'Bonjour cher ami(e). Je suis le bot Moustapha.')
+  elsif message.text.include? "Bonne nuit"
+  	message.reply(text: 'Haa c\'est vrai il fait tard. bonne nuit à toi aussi.')
+  else
+  	message.reply(text: 'Am robot Moustapha. Am not super clever yet. come back later hahaa.')
+  end
 end
 # subscribed_fields: %w[feed mention name]
